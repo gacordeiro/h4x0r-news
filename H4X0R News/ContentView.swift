7c56cbcc337f6521a52fa7b966e57b07dbdbe893
Guilherme Cordeiro <guilherme.cordeiro@luizalabs.com>
2020-02-04 07:45:26 -0300

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by gacordeiro LuizaLabs on 04/02/20.
+//  Copyright © 2020 Tutuland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
